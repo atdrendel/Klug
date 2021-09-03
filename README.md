929f61a522159707b57381cd44dfe51e89cc39ca
codebendr-product-designer <codebendr@gmail.com>
2021-09-03 13:11:33 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Klug
+
+A description of this package.
---
+++ Tests/KlugTests/KlugTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Klug
+
+final class KlugTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Klug().text, "Hello, World!")
+    }
+}
