9f42f926f5ac12258dda5d7d40539357b23fd88e
codebendr-product-designer <codebendr@gmail.com>
2021-10-02 21:52:06 +0000

    exploring view conformance

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -1,16 +1,27 @@
 import SwiftUI
 
-public extension Klug {
 
-    typealias IconView = HStack<TupleView<(Image, TextField<Text>, AnyView)>>
+public extension Klug {
+    
+    enum AccessoryViewType {
+        case password(_ action: (Bool) -> ())
+        case custom(_ view: AnyView)
+        case none
+    }
+    
+    typealias IconView = TupleView<(Image, TextField<Text>, AccessoryViewType)>
+    
+    enum ViewType {
+        case stack
+    }
 }
 
 public extension HStack where Content == Klug.IconView {
      init(
-        spacing: CGFloat? = nil,
+        type: Klug.ViewType,
         @ViewBuilder content: () -> Content
     ) {
-        self.init(alignment: .center, spacing: spacing, content: content)
+        self.init(alignment: .center, spacing: 10, content: content)
     }
 }
 
