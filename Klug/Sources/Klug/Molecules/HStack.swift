7ed8a70fa353764c9a6b8e8daf1643d067ded4cd
codebendr-product-designer <codebendr@gmail.com>
2021-10-03 00:33:27 +0000

    extracted custom view and password view into its own file

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -6,38 +6,10 @@ import Relative
 
 public extension Klug {
     
-    struct CustomView<Content: View>: View {
-        let content: Content
-
-        init(@ViewBuilder content: () -> Content) {
-            self.content = content()
-        }
-        
-        public var body: some View {
-            content
-        }
-    }
-    
-    private struct PasswordView: View {
-        var action: (Bool) -> ()
-        @State private var isPasswordShowing = false
-        
-        public var body: some View {
-            Group {
-                isPasswordShowing ? Image(systemName: "eye.slash") : Image(systemName: "eye")
-            }
-            .onTapGesture {
-                isPasswordShowing.toggle()
-                action(isPasswordShowing)
-            }
-        }
-    }
-    
     enum AccessoryViewType: View {
         case password(_ action: (Bool) -> ())
         case custom
         case none
-      //  @State private password = false
         public var body: some View {
             switch self {
             case let .password(action):
