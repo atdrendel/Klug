4240c2ad09daa51bcbe42632fa864456dbfc9f28
codebendr-product-designer <codebendr@gmail.com>
2021-10-03 10:46:03 +0000

    updated HStack custom implementation to make more sense

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
-
 public extension HStack where Content == Klug.IconView {
-     init(
-        type: Klug.ViewType,
-        @ViewBuilder content: () -> Content
-    ) {
-        self.init(alignment: .center, spacing: 10, content: content)
+    init(icon: Image, mainView: TextField<Text>, acccessoryView: Klug.AccessoryViewType) {
+        self.init(alignment: .center, spacing: 10) {
+            icon
+            mainView
+            acccessoryView
+        }
     }
 }
 
