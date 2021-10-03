016473e7bf0a4d38520ba1e226acdf276928528a
codebendr-product-designer <codebendr@gmail.com>
2021-10-03 00:58:40 +0000

    moved klug extension out of HStack file

--- Klug/Sources/Klug/Atom/CustomView.swift
+++ Klug/Sources/Klug/Atom/CustomView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 public extension Klug {
+    
     struct CustomView<Content: View>: View {
         let content: Content
 
@@ -27,4 +28,28 @@ public extension Klug {
             }
         }
     }
+}
+
+public extension Klug {
+
+    typealias IconView<Accessory: View> = TupleView<(Image, TextField<Text>, AccessoryViewType<Accessory>)>
+    
+    enum ViewType {
+        case stack
+    }
+    
+    enum AccessoryViewType<Accessory: View>: View {
+        case password(_ action: (Bool) -> ())
+        case custom(_ view: Accessory )
+        case none
+        public var body: some View {
+            switch self {
+            case let .password(action):
+                PasswordView(action: action)
+            case let .custom(view): view
+            case .none: EmptyView()
+            }
+        }
+    }
+
 }
--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -2,32 +2,6 @@ import SwiftUI
 import Relative
 
 
-public extension Klug {
-    Pas
-    enum AccessoryViewType: View {
-        case password(_ action: (Bool) -> ())
-        case custom
-        case none
-        public var body: some View {
-            switch self {
-            case let .password(action):
-                PasswordView(action: action)
-            case .custom:
-                CustomView {
-                    Text("")
-                }
-            case .none: EmptyView()
-            }
-        }
-    }
-    
-    typealias IconView = TupleView<(Image, TextField<Text>, AccessoryViewType)>
-    
-    enum ViewType {
-        case stack
-    }
-}
-
 public extension HStack where Content == Klug.IconView {
      init(
         type: Klug.ViewType,
