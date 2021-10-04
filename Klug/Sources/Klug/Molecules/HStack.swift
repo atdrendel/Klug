73ddd8ceacd946ccd67f79d8c01415a4fc303b53
codebendr-product-designer <codebendr@gmail.com>
2021-10-04 21:33:12 +0000

    added conditional text to be able to switch between secure text field and text field

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -1,31 +1,37 @@
 import SwiftUI
-import Relative
 
 public extension HStack where Content == Klug.AccessoryView {
     init(
         _ image: Image,
-        _ isSecureText: Bool,
+        _ isSecureText: Binding<Bool>,
         _ textfield: (titleKey: LocalizedStringKey, text: Binding<String>, prompt: Text?),
         _ accessoryView: Klug.AccessoryViewType = .none
     ) {
         typealias _conditionalText = _ConditionalContent<SecureField<Text>, TextField<Text>>
-        func _textfield() ->  _conditionalText {
-            let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
-            let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
-            
-            let trueContent: _conditionalText =
-            ViewBuilder.buildEither(first: secureTextField)
-            
-            let falseContent: _conditionalText =
-            ViewBuilder.buildEither(second: textField)
-            let _text: _conditionalText =
-            isSecureText ? trueContent : falseContent
-            return _text
-        }
+            func _textfield() ->  _conditionalText {
+                let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+                let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+                
+                let trueContent: _conditionalText =
+                ViewBuilder.buildEither(first: secureTextField)
+                
+                let falseContent: _conditionalText =
+                ViewBuilder.buildEither(second: textField)
+                let _text: _conditionalText =
+                isSecureText.wrappedValue ? trueContent : falseContent
+                return _text
+            }
         self.init(alignment: .center, spacing: 10) {
             image
             _textfield()
             accessoryView
         }
+    }
+}
+
+struct HStack_Preview: PreviewProvider {
+    @State static var isSecureText = true
+    static var previews: some View {
+        HStack(.init(systemName: "lock"), $isSecureText, (titleKey: "some text", text: .constant(""), prompt: nil), .password({isSecureText = $0}))
     }
 }
