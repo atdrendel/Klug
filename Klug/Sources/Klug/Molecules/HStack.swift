74efde330c91b5cfb05e3121a74f477b857bf6aa
codebendr-product-designer <codebendr@gmail.com>
2021-10-04 15:57:49 +0000

    added conditional type to choose between secure text field and text field

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -1,15 +1,30 @@
 import SwiftUI
-
+import Relative
 
 public extension HStack where Content == Klug.AccessoryView {
-     init(
+    init(
         _ image: Image,
-        _ textfield: TextField<Text>,
+        _ isSecureText: Bool,
+        _ textfield: (titleKey: LocalizedStringKey, text: Binding<String>, prompt: Text?),
         _ accessoryView: Klug.AccessoryViewType = .none
     ) {
+        typealias _conditionalText = _ConditionalContent<SecureField<Text>, TextField<Text>>
+        func _textfield() ->  _conditionalText {
+            let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+            let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+            
+            let trueContent: _conditionalText =
+            ViewBuilder.buildEither(first: secureTextField)
+            
+            let falseContent: _conditionalText =
+            ViewBuilder.buildEither(second: textField)
+            let _text: _conditionalText =
+            isSecureText ? trueContent : falseContent
+            return _text
+        }
         self.init(alignment: .center, spacing: 10) {
             image
-            textfield
+            _textfield()
             accessoryView
         }
     }
