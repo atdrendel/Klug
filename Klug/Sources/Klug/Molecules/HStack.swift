8b7a0fd0e2fa8b85abf8db91cd9e44eaf464262e
codebendr-product-designer <codebendr@gmail.com>
2021-10-05 00:20:26 +0000

    added conditional text and text field image modifier

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -8,16 +8,17 @@ public extension HStack where Content == Klug.AccessoryView {
         _ accessoryView: Klug.AccessoryViewType = .none
     ) {
         typealias _conditionalText = _ConditionalContent<SecureField<Text>, TextField<Text>>
-            func _textfield() ->  _conditionalText {
-                let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
-                let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
-                
-                return isSecureText.wrappedValue ?
-                ViewBuilder.buildEither(first: secureTextField) :
-                ViewBuilder.buildEither(second: textField)
-            }
+        func _textfield() ->  _conditionalText {
+            let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+            let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+            
+            return isSecureText.wrappedValue ?
+            ViewBuilder.buildEither(first: secureTextField) :
+            ViewBuilder.buildEither(second: textField)
+        }
+        
         self.init(alignment: .center, spacing: 10) {
-            image
+            image.modifier(Klug.TextFieldImageModifier())
             _textfield()
             accessoryView
         }
