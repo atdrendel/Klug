24b8f4856359d79eb55b0390f8293f0cbfe93c21
codebendr-product-designer <codebendr@gmail.com>
2021-10-07 00:08:08 +0000

    implemented inventory view and view model

--- KlugNavigation/ContentView.swift
+++ KlugNavigation/ContentView.swift
@@ -8,45 +8,52 @@
 import SwiftUI
 
 enum Tab {
-  case one, two, three
+  case one, inventory, three
 }
 
 class AppViewModel: ObservableObject {
-    @Published var selectedTab: Tab
+  @Published var inventoryViewModel: InventoryViewModel
+  @Published var selectedTab: Tab
 
-    init(selectedTab: Tab = .one) {
-      self.selectedTab = selectedTab
-    }
+  init(
+    inventoryViewModel: InventoryViewModel = .init(),
+    selectedTab: Tab = .one
+  ) {
+    self.inventoryViewModel = inventoryViewModel
+    self.selectedTab = selectedTab
+  }
+
+ 
 }
 
 struct ContentView: View {
     @ObservedObject var viewModel: AppViewModel
     
     var body: some View {
-        var body: some View {
+    
             TabView(selection: $viewModel.selectedTab) {
                 
                 VStack {
                   Text("One")
-                  Button("Go to 2nd tab") { self.viewModel.selectedTab = .two }
+                  Button("Go to 2nd tab") { self.viewModel.selectedTab = .inventory }
                 }
                 .tabItem { Text("One") }
                 .tag(Tab.one)
                 
-                Text("Two")
-                    .tabItem { Text("Two") }
-                    .tag(Tab.two)
+                InventoryView(viewModel: self.viewModel.inventoryViewModel)
+                  .tabItem { Text("Two") }
+                  .tag(Tab.inventory)
                 
                 Text("Three")
                     .tabItem { Text("Three") }
                     .tag(Tab.three)
             }
-        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(selection: .init(selectedTab: .two))
+        ContentView(viewModel: .init(selectedTab: .inventory))
     }
 }
