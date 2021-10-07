08cd041ca9359a9431ebde7e510e10c8c13d0b95
codebendr-product-designer <codebendr@gmail.com>
2021-10-07 00:30:41 +0000

    added delete to inventory view model

--- KlugNavigation/KlugNavigationApp.swift
+++ KlugNavigation/KlugNavigationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KlugNavigationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: .init(selectedTab: .inventory))
         }
     }
 }
