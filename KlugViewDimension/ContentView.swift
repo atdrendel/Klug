52599506506c2fc6fa94f13d6d274c95ed1427ae
codebendr-product-designer <codebendr@gmail.com>
2021-10-13 21:18:29 +0000

    testing layout prioriites

--- KlugViewDimension/ContentView.swift
+++ KlugViewDimension/ContentView.swift
@@ -33,17 +33,23 @@ struct ContentView: View {
 //                    .mask(Capsule())
 //            )
             
-            MeasureBehavior(
-                HStack {
-                    Text("Hello, World, World")
-                        .layoutPriority(1)
-                        .lineLimit(1)
-                        .truncationMode(.middle)
-                    Rectangle().fill(.blue).frame(minWidth: 50)
-                }
-                    
-            )
+//            MeasureBehavior(
+//                HStack {
+//                    Text("Hello, World, World")
+//                        .layoutPriority(1)
+//                        .lineLimit(1)
+//                        .truncationMode(.middle)
+//                    Rectangle().fill(.blue).frame(minWidth: 50)
+//                }
+//
+//            )
             
+            MeasureBehavior(
+                HStack(spacing: 0) {
+                Text("some text some text some text some text some text").truncationMode(.middle).lineLimit(1)
+                Text("chapter1.md").layoutPriority(1)
+                }
+            )
             
  
         }
