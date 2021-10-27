e880ce5fe0ff7fa4bc5da37e3a330b99b035638e
codebendr-product-designer <codebendr@gmail.com>
2021-10-27 03:47:41 +0000

    added static data to app init

--- KlugNavigation/KlugNavigationApp.swift
+++ KlugNavigation/KlugNavigationApp.swift
@@ -10,8 +10,22 @@ import SwiftUI
 @main
 struct KlugNavigationApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView(viewModel: .init(selectedTab: .inventory))
-        }
+      let keyboard = Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+
+      WindowGroup {
+        ContentView(
+          viewModel: .init(
+            inventoryViewModel: .init(
+              inventory: [
+                .init(item: keyboard, route: .deleteAlert),
+                .init(item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))),
+                .init(item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))),
+                .init(item: Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false))),
+              ]
+            ),
+            selectedTab: .inventory
+          )
+        )
+      }
     }
 }
