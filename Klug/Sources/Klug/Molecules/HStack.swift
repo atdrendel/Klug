ff2f3162feae8b612da641afe1279c6062429d50
codebendr-product-designer <codebendr@gmail.com>
2021-11-03 21:05:04 +0000

    tested how to use custom initializer with specific view change recommendations

--- Klug/Sources/Klug/Molecules/HStack.swift
+++ Klug/Sources/Klug/Molecules/HStack.swift
@@ -1,6 +1,27 @@
 import SwiftUI
 
+typealias _conditionalText = _ConditionalContent<SecureField<Text>, TextField<Text>>
 
+func _textfield(_ isSecureText: Binding<Bool>, _ textfield: (titleKey: LocalizedStringKey, text: Binding<String>, prompt: Text?)) ->  _conditionalText {
+    let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+    let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+    
+    return isSecureText.wrappedValue ?
+    ViewBuilder.buildEither(first: secureTextField) :
+    ViewBuilder.buildEither(second: textField)
+}
+
+extension Klug {
+public struct _TextField {
+    func _textfield(_ isSecureText: Binding<Bool>, _ textfield: (titleKey: LocalizedStringKey, text: Binding<String>, prompt: Text?)) ->  _conditionalText {
+        let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+        let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
+        return isSecureText.wrappedValue ?
+        ViewBuilder.buildEither(first: secureTextField) :
+        ViewBuilder.buildEither(second: textField)
+    }
+}
+}
 public extension HStack where Content == Klug.AccessoryView {
     init(
         _ image: Image,
@@ -8,20 +29,24 @@ public extension HStack where Content == Klug.AccessoryView {
         _ textfield: (titleKey: LocalizedStringKey, text: Binding<String>, prompt: Text?),
         _ accessoryView: Klug.AccessoryViewType = .none
     ) {
-        typealias _conditionalText = _ConditionalContent<SecureField<Text>, TextField<Text>>
-        func _textfield() ->  _conditionalText {
-            let textField = TextField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
-            let secureTextField = SecureField(textfield.titleKey, text: textfield.text, prompt: textfield.prompt)
-            
-            return isSecureText.wrappedValue ?
-            ViewBuilder.buildEither(first: secureTextField) :
-            ViewBuilder.buildEither(second: textField)
-        }
-        
         self.init(alignment: .center, spacing: 10) {
             image.modifier(Klug.TextFieldImageModifier())
-            _textfield()
+            _textfield(isSecureText, textfield)
             accessoryView
+        }
+    }
+}
+public extension HStack where Content == Klug.AccessoryViewImage {
+    init(
+        _ image: Image,
+        _ isSecureText: Binding<Bool>,
+        _ textfield: (titleKey: LocalizedStringKey, text: Binding<String>, prompt: Text?),
+        _ imageAccessory: Image
+    ) {
+        self.init(alignment: .center, spacing: 10) {
+            image.modifier(Klug.TextFieldImageModifier())
+            _textfield(isSecureText, textfield)
+            imageAccessory
         }
     }
 }
