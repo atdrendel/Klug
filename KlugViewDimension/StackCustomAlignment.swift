124fc2756168d9c51b75712d8bdf3f82af15e86e
codebendr-product-designer <codebendr@gmail.com>
2021-11-07 19:04:01 +0000

    custom VerticalAlignment

--- KlugViewDimension/StackCustomAlignment.swift
+++ KlugViewDimension/StackCustomAlignment.swift
@@ -15,6 +15,11 @@ struct StackCustomAlignment: View {
         HStack(alignment: .myCenter) {
             Rectangle().fill(Color.blue).frame(width: 50, height: 50)
             Rectangle().fill(Color.green).frame(width: 30, height: 30)
+            Rectangle().fill(Color.red)
+            .frame(width: 40, height: 40)
+            .alignmentGuide(.myCenter, computeValue: {
+                 $0[.myCenter] - 20
+            })
         }
     }
 }
