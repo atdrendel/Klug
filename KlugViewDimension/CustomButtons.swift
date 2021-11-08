dbf7cfb75646cb40ab137b28ccaad39702777848
codebendr-product-designer <codebendr@gmail.com>
2021-11-08 13:56:19 +0000

    added a button style

--- KlugViewDimension/CustomButtons.swift
+++ KlugViewDimension/CustomButtons.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct CustomButtons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+        Button("One", action: {})
+        Button("Two", action: {})
+        Button("Three", action: {})
+        }.buttonStyle(CircleStyle())
+    }
+}
+
+struct CircleStyle: ButtonStyle {
+    var foreground = Color.white
+    var background = Color.blue
+    func makeBody(configuration: ButtonStyleConfiguration) -> some View {
+        Circle()
+            .fill(background.opacity(configuration.isPressed ? 0.8 : 1))
+            .overlay(Circle().strokeBorder(foreground).padding(3))
+            .overlay(configuration.label.foregroundColor(foreground))
+            .frame(width: 75, height: 75)
     }
 }
 
