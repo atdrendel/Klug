a17641bf29dc8b73ce21dca32fd205b0c145e11f
codebendr-product-designer <codebendr@gmail.com>
2021-11-08 13:25:58 +0000

    added CustomButtons

---
+++ KlugViewDimension/CustomButtons.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButtons.swift
+//  KlugViewDimension
+//
+//  Created by Evans Domina Attafuah on 08/11/2021.
+//
+
+import SwiftUI
+
+struct CustomButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtons()
+    }
+}
