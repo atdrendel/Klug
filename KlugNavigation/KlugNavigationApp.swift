acb9e9956227ec56511c94a76f5e87d6f17e7335
codebendr-product-designer <codebendr@gmail.com>
2021-11-11 09:58:17 +0000

    added deep linking

--- KlugNavigation/KlugNavigationApp.swift
+++ KlugNavigation/KlugNavigationApp.swift
@@ -3,14 +3,18 @@ import SwiftUI
 @main
 struct KlugNavigationApp: App {
     var body: some Scene {
-      let keyboard = Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        let keyboard = Item(name: "Keyboard", color: .blue, status: .inStock(quantity: 100))
+        
+        var editedKeyBoard = keyboard
+        editedKeyBoard.name = "Bluetooth Keyboard"
+        editedKeyBoard.status = .inStock(quantity: 1000)
 
-      WindowGroup {
+      return WindowGroup {
         ContentView(
           viewModel: .init(
             inventoryViewModel: .init(
               inventory: [
-                .init(item: keyboard, route: .deleteAlert),
+                .init(item: keyboard, route: .edit(editedKeyBoard)),
                 .init(item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))),
                 .init(item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))),
                 .init(item: Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false))),
