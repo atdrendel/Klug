cffe2469f504e5177e0fef249c34c135a0ac2b19
codebendr-product-designer <codebendr@gmail.com>
2021-12-09 04:32:11 +0000

    added a new project

---
+++ KlugDesign/KlugDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlugDesignApp.swift
+//  KlugDesign
+//
+//  Created by Evans Domina Attafuah on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct KlugDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
