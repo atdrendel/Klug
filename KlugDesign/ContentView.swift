91e24c18a3d2fc67419e5f7c515678127fb8eefb
codebendr-product-designer <codebendr@gmail.com>
2021-12-09 05:06:38 +0000

    added a Stack layout with images and text

--- KlugDesign/ContentView.swift
+++ KlugDesign/ContentView.swift
@@ -2,8 +2,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+            .fontWeight(.bold)
+            
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text(/*@START_MENU_TOKEN@*/"Build an iOS app for iOS 15 with custom layouts, animations and ..."/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+        
     }
 }
 
