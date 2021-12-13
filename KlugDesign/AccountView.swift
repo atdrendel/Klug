2a23e55638fb72e7947e691367e5ef7eae1bed92
codebendr-product-designer <codebendr@gmail.com>
2021-12-13 07:39:28 +0000

    added symbol variant and style

--- KlugDesign/AccountView.swift
+++ KlugDesign/AccountView.swift
@@ -11,10 +11,32 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                
+                Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                    .symbolVariant(.circle.fill)
+                    .font(.system(size: 32))
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.blue, .blue.opacity(0.3))
+                    .padding()
+                    .background(Circle().fill(.ultraThinMaterial))
+                    .background(
+                        Image(systemName: "hexagon")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.blue)
+                            .font(.system(size: 200))
+                            .offset(x: -50, y: -100)
+                    )
+                
+                
                 Section {
                     Text("Settings")
                     Text("Billing")
                     Text("Help")
+//                    Image(systemName: "person.3.sequence")
+//                        .symbolVariant(.fill)
+//                        .symbolRenderingMode(.palette)
+//                        .foregroundStyle(.yellow, .red, .teal)
+//                        .font(.largeTitle)
                 }
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
