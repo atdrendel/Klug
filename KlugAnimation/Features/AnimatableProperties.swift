6ee2679a07214252813c3969d06e7eb00cc6fb70
codebendr-product-designer <codebendr@gmail.com>
2021-12-13 06:01:23 +0000

    added different animatable properties.

--- KlugAnimation/Features/AnimatableProperties.swift
+++ KlugAnimation/Features/AnimatableProperties.swift
@@ -1,9 +1,16 @@
+import Klug
 import SwiftUI
 
 struct HueView: View {
     @State var change = false
     var body: some View {
-        VStack {
+        HStack {
+            // animate alignment
+            Text("Animate Text Alignment")
+                .multilineTextAlignment(change ?
+                    .trailing : .leading)
+                .frame(width: .infinity)
+            
             // Hue Saturation
             Circle()
                 .fill(.pink)
@@ -13,20 +20,61 @@ struct HueView: View {
                 .onTapGesture {
                     change.toggle()
                 }
-
-            //Offset
+            
+            // Offset
             Circle()
                 .fill(.blue)
                 .hueRotation(.init(degrees: change ? 170 : 0))
                 .frame(width: 50)
-                .offset(x: change ? 140 : -140, y: 0)
+                // .offset(x: change ? 140 : -140, y: 0)
                 .animation(.default, value: change)
+            //            Spacer()
         }
+    }
+}
+
+struct Animate_Alignment: View {
+    @State private var change = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            VStack(alignment: change ? .trailing : .leading) {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.blue)
+                    .frame(width: 100, height: 100)
+                    .animation(.default, value: change)
+                HStack {
+                    Spacer()
+                }
+            }
+            
+            HStack(alignment: change ? .bottom : .top) {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundColor(.blue)
+                    .frame(width: 100, height: 100)
+                    .animation(.default, value: change)
+                VStack {
+                    Spacer()
+                }
+            }.frame(maxHeight: .infinity)
+            
+            Button("Change") {
+                change.toggle()
+            }
+            
+            GeometryReader { gp in
+                RoundedRectangle(cornerRadius:20)
+                .foregroundColor(.blue)
+                .frame(width: 100, height: 100)
+                .position(change ? CGPoint(x: 50, y: 50) : CGPoint(x: gp.size.width - 50, y: gp.size.height - 50))
+                .animation(.default, value: change)
+            }
+        }.font(.title)
     }
 }
 
 struct HueViewPreview: PreviewProvider {
     static var previews: some View {
-        HueView()
+        Animate_Alignment()
     }
 }
