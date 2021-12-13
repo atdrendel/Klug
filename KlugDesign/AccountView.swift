7fdffba82ba4243cfe69432f0b1cba4f1f51d541
codebendr-product-designer <codebendr@gmail.com>
2021-12-13 07:54:22 +0000

    added customisation for SF Symbols

--- KlugDesign/AccountView.swift
+++ KlugDesign/AccountView.swift
@@ -12,31 +12,46 @@ struct AccountView: View {
         NavigationView {
             List {
                 
-                Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                    .symbolVariant(.circle.fill)
-                    .font(.system(size: 32))
-                    .symbolRenderingMode(.palette)
-                    .foregroundStyle(.blue, .blue.opacity(0.3))
-                    .padding()
-                    .background(Circle().fill(.ultraThinMaterial))
-                    .background(
-                        Image(systemName: "hexagon")
-                            .symbolVariant(.fill)
-                            .foregroundStyle(.blue)
-                            .font(.system(size: 200))
-                            .offset(x: -50, y: -100)
+                VStack {
+                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundStyle(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
                     )
-                
+                    
+                    Text("Codebendr")
+                        .font(.title.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location")
+                            .imageScale(.small)
+                        Text("Canada")
+                            .foregroundColor(.secondary)
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
                 
                 Section {
-                    Text("Settings")
-                    Text("Billing")
-                    Text("Help")
-//                    Image(systemName: "person.3.sequence")
-//                        .symbolVariant(.fill)
-//                        .symbolRenderingMode(.palette)
-//                        .foregroundStyle(.yellow, .red, .teal)
-//                        .font(.largeTitle)
+                    Label("Settings", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
+                        .imageScale(.large)
+          
+                    Image(systemName: "person.3.sequence")
+                        .symbolVariant(.fill)
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.yellow, .red, .teal)
+                        .font(.largeTitle)
                 }
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
