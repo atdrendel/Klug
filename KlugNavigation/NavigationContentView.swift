b7a05f1b9818b9507ea71cab67014c8ee4863b1b
codebendr-product-designer <codebendr@gmail.com>
2021-12-18 18:33:18 +0000

    added basic deep linking to app

--- KlugNavigation/NavigationContentView.swift
+++ KlugNavigation/NavigationContentView.swift
@@ -1,48 +1,63 @@
 import SwiftUI
 
 enum Tab {
-  case one, inventory, three
+    case one, inventory, three
 }
 
 class AppViewModel: ObservableObject {
-  @Published var inventoryViewModel: InventoryViewModel
-  @Published var selectedTab: Tab
+    @Published var inventoryViewModel: InventoryViewModel
+    @Published var selectedTab: Tab
 
-  init(
-    inventoryViewModel: InventoryViewModel = .init(),
-    selectedTab: Tab = .one
-  ) {
-    self.inventoryViewModel = inventoryViewModel
-    self.selectedTab = selectedTab
-  }
+    init(
+        inventoryViewModel: InventoryViewModel = .init(),
+        selectedTab: Tab = .one
+    ) {
+        self.inventoryViewModel = inventoryViewModel
+        self.selectedTab = selectedTab
+    }
+
+    func open(url: URL) {
+        switch url.path {
+        case "/one":
+            self.selectedTab = .one
+        case "/inventory":
+            self.selectedTab = .inventory
+        case "/three":
+            self.selectedTab = .three
+        default: break
+        }
+    }
 }
 
 struct NavigationContentView: View {
-  @ObservedObject var viewModel: AppViewModel
+    @ObservedObject var viewModel: AppViewModel
 
-  var body: some View {
-    TabView(selection: self.$viewModel.selectedTab) {
-      Button("Go to 2nd tab") {
-        self.viewModel.selectedTab = .inventory
-      }
-        .tabItem { Text("One") }
-        .tag(Tab.one)
+    var body: some View {
+        TabView(selection: self.$viewModel.selectedTab) {
+            Button("Go to 2nd tab") {
+                self.viewModel.selectedTab = .inventory
+            }
+            .tabItem { Text("One") }
+            .tag(Tab.one)
 
-      NavigationView {
-        InventoryView(viewModel: self.viewModel.inventoryViewModel)
-      }
-        .tabItem { Text("Inventory") }
-        .tag(Tab.inventory)
+            NavigationView {
+                InventoryView(viewModel: self.viewModel.inventoryViewModel)
+            }
+            .tabItem { Text("Inventory") }
+            .tag(Tab.inventory)
 
-      Text("Three")
-        .tabItem { Text("Three") }
-        .tag(Tab.three)
+            Text("Three")
+                .tabItem { Text("Three") }
+                .tag(Tab.three)
+        }
+        .onOpenURL { url in
+            self.viewModel.open(url: url)
+        }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-      NavigationContentView(viewModel: .init(selectedTab: .inventory))
-  }
+    static var previews: some View {
+        NavigationContentView(viewModel: .init(selectedTab: .inventory))
+    }
 }
