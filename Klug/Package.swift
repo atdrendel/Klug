e638650471608b88d3d4f13493e4eda72b68ba0b
codebendr-product-designer <codebendr@gmail.com>
2021-12-21 03:01:50 +0000

    added `InventoryViewModel` and `InventoryViewController` to Inventory Feature

--- Klug/Package.swift
+++ Klug/Package.swift
@@ -6,22 +6,31 @@ let package = Package(
     name: "Klug",
     platforms: [.iOS(.v15)],
     products: [
+        .library(name: "InventoryFeature", targets: ["InventoryFeature"]),
+        .library(name: "ItemFeature", targets: ["ItemFeature"]),
+        .library(name: "ItemRowFeature", targets: ["ItemRowFeature"]),
         .library(name: "Klug", targets: ["Klug"]),
         .library(name: "Models", targets: ["Models"]),
         .library(name: "ParsingHelpers", targets: ["ParsingHelpers"]),
         .library(name: "SwiftUIHelpers", targets: ["SwiftUIHelpers"]),
-        .library(name: "ItemFeature", targets: ["ItemFeature"]),
-        .library(name: "ItemRowFeature", targets: ["ItemRowFeature"])
-
     ],
     dependencies: [
         //   .package(name: "Relative", path: "../../Relative")
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.7.0"),
+        .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "0.3.2"),
         .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.4.0")
     ],
     targets: [
         .target(name: "Klug"),
         .target(name: "Models"),
+        .target(name: "InventoryFeature",
+                dependencies: [
+                    "ItemRowFeature",
+                    "Models",
+                    "SwiftUIHelpers",
+                    .product(name: "CasePaths", package: "swift-case-paths"),
+                    .product(name: "IdentifiedCollections", package: "swift-identified-collections")
+                ]),
         .target(name: "ItemFeature",
                 dependencies: [
                     "Models",
