1c5515a39d7b8060f52676355bfee8595a0d27c4
codebendr-product-designer <codebendr@gmail.com>
2021-12-22 05:13:32 +0000

    added habit tracker system colours

--- Klug/Sources/Klug/Atom/Colors.swift
+++ Klug/Sources/Klug/Atom/Colors.swift
@@ -1,23 +1,6 @@
-//
-//  File.swift
-//  File
-//
-//  Created by Evans Domina Attafuah on 01/09/2021.
-//
-import UIKit
-
-public extension UIColor {
-    static let primary = #colorLiteral(red: 0.1568627451, green: 0.3725490196, blue: 0.6235294118, alpha: 1)
-    static let accent = #colorLiteral(red: 0.5237309933, green: 0.7208840251, blue: 0.8984263539, alpha: 1)
-    static let secondary = #colorLiteral(red: 0.9371759295, green: 0.9373072386, blue: 0.9371345043, alpha: 1)
-    static let alt = #colorLiteral(red: 0.8077545762, green: 0.8078941703, blue: 0.8077457547, alpha: 1)
-    static let alert = #colorLiteral(red: 0.9017465711, green: 0.5274231434, blue: 0.5234265327, alpha: 1)
-    static let vulcan = #colorLiteral(red: 0.08235294118, green: 0.07843137255, blue: 0.09803921569, alpha: 1)
-    static let battleShipGray = #colorLiteral(red: 0.5176470588, green: 0.5176470588, blue: 0.5176470588, alpha: 1)
-    static let frenchGray = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1)
-    static let valencia = #colorLiteral(red: 0.8637464643, green: 0.3055342138, blue: 0.2557086349, alpha: 1)
-    static let superNova = #colorLiteral(red: 1, green: 0.7333333333, blue: 0.007843137255, alpha: 1)
-    static let magnesium = #colorLiteral(red: 0.7019607843, green: 0.7019607843, blue: 0.7019607843, alpha: 1)
-    static let wildSand = #colorLiteral(red: 0.9568627451, green: 0.9568627451, blue: 0.9568627451, alpha: 1)
-    static let charcoal = #colorLiteral(red: 0.2745098039, green: 0.2745098039, blue: 0.2745098039, alpha: 1)
+public extension Color {
+    static let blue = #colorLiteral(red: 0.1449999958, green: 0.1490000039, blue: 0.2509999871, alpha: 1)
+    static let darkGray = #colorLiteral(red: 0.4269999862, green: 0.451000005, blue: 0.4979999959, alpha: 1)
+    static let gray = #colorLiteral(red: 0.8669999838, green: 0.8820000291, blue: 0.8939999938, alpha: 1)
+    statuc let green = #colorLiteral(red: 0.2820000052, green: 0.4589999914, blue: 0.3880000114, alpha: 1)
 }
