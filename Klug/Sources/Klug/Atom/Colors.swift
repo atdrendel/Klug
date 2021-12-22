fd284c78e234b1a13c53c47dc7c0e203e2a8e1a8
codebendr-product-designer <codebendr@gmail.com>
2021-12-22 09:30:05 +0000

    added rest of colours.

--- Klug/Sources/Klug/Atom/Colors.swift
+++ Klug/Sources/Klug/Atom/Colors.swift
@@ -2,5 +2,11 @@ public extension Color {
     static let blue = #colorLiteral(red: 0.1449999958, green: 0.1490000039, blue: 0.2509999871, alpha: 1)
     static let darkGray = #colorLiteral(red: 0.4269999862, green: 0.451000005, blue: 0.4979999959, alpha: 1)
     static let gray = #colorLiteral(red: 0.8669999838, green: 0.8820000291, blue: 0.8939999938, alpha: 1)
-    statuc let green = #colorLiteral(red: 0.2820000052, green: 0.4589999914, blue: 0.3880000114, alpha: 1)
+    static let green = #colorLiteral(red: 0.2820000052, green: 0.4589999914, blue: 0.3880000114, alpha: 1)
+    static let lightBlue = #colorLiteral(red: 0.7179999948, green: 0.8709999919, blue: 0.9060000181, alpha: 1)
+    static let pink = #colorLiteral(red: 0.9409999847, green: 0.824000001, blue: 0.8309999704, alpha: 1)
+    static let purple = #colorLiteral(red: 0.8590000272, green: 0.8309999704, blue: 1, alpha: 1)
+    static let seaBlue = #colorLiteral(red: 0.8629999757, green: 0.9330000281, blue: 0.9060000181, alpha: 1)
+    static let yellow =  #colorLiteral(red: 0.9610000253, green: 0.8709999919, blue: 0.5220000148, alpha: 1)
 }
+
