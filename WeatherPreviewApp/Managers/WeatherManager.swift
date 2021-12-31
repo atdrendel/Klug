d1122423016deef4183719811b0eb8badeaf5936
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2021-12-31 08:56:14 +0000

    removed open weather from repo

--- WeatherPreviewApp/Managers/WeatherManager.swift
+++ WeatherPreviewApp/Managers/WeatherManager.swift
@@ -2,7 +2,7 @@ import CoreLocation
 
 struct WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
-        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=15a5654a58f6e0dbd6b1ee160786128a&units=metric") else { fatalError("Missing URL") }
+        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(.weatherKey)&units=metric") else { fatalError("Missing URL") }
         
         let urlRequest = URLRequest(url: url)
         
