7bcad87db0a5bf2b877f4d7a8d9a38ed4550519b
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2021-12-31 09:26:15 +0000

    added static data for previews

---
+++ WeatherPreviewApp/Extensions.swift
@@ -0,0 +1,5 @@
+extension Double {
+    func round() -> String {
+        .init(format: "%.0f", self)
+    }
+}
---
+++ WeatherPreviewApp/Views/WeatherView.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherView.swift
+//  WeatherPreviewApp
+//
+//  Created by Evans Domina Attafuah on 31/12/2021.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
