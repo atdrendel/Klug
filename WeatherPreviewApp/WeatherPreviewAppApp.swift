4b3f1720aa78f7caaac65d3f2486f05aaecfe102
codebendr-product-designer <codebendr@gmail.com>
2021-12-31 06:49:08 +0000

    added a weather preview app and some assets

---
+++ WeatherPreviewApp/WeatherPreviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPreviewAppApp.swift
+//  WeatherPreviewApp
+//
+//  Created by Evans Domina Attafuah on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPreviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
