b5e10b9d07eb09a209711936b9e64afbf6e888ba
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2021-12-31 08:25:19 +0000

    added a singleton of URLSession to retrieve server response with its optional network data

--- WeatherPreviewApp/Managers/WeatherManager.swift
+++ WeatherPreviewApp/Managers/WeatherManager.swift
@@ -2,6 +2,12 @@ import CoreLocation
 
 struct WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
-        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=15a5654a58f6e0dbd6b1ee160786128a") else { fatalError("Missing URL") }
+        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=15a5654a58f6e0dbd6b1ee160786128a&units=metric") else { fatalError("Missing URL") }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data") }
     }
 }
