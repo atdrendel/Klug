82ab97c07c86e4985953368e9179a0db3c38a8a2
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-01-02 02:48:49 +0000

    constructing Most Popular Habits View

--- HabitTrackerPreviewApp/PopularHabitView.swift
+++ HabitTrackerPreviewApp/PopularHabitView.swift
@@ -1,42 +1,75 @@
 import SwiftUI
 
 public struct TitleFirstLabelStyle: LabelStyle {
-    
     public init() {}
 
     public func makeBody(configuration: Configuration) -> some View {
         HStack {
             configuration.title
+            Spacer()
             configuration.icon
         }
+        .padding(.top, 10)
+        .padding(.bottom, 10)
+        .font(.title3.bold())
+        .foregroundColor(Color("blue"))
     }
 }
 
-//func labelStyle<S>(_ style: S) -> some View where S : LabelStyle
-extension LabelStyle where Self == TitleFirstLabelStyle {
-    public static var titleFirst: Self { .init() }
+// func labelStyle<S>(_ style: S) -> some View where S : LabelStyle
+public extension LabelStyle where Self == TitleFirstLabelStyle {
+    static var titleFirst: Self { .init() }
 }
 
 struct PopularHabitView: View {
     var body: some View {
-        HStack {
+        VStack {
             HStack {
+                
                 Button {} label: {
                     Label {
-                        Text("Hello")
+                        Text("Meditation")
                     } icon: {
-                        Image(systemName: "heart")
+                        Image(systemName: "aqi.medium")
                     }
-                    .labelStyle(.titleFirst)
+                  
                 }
+                .tint(Color("seaBlue"))
+                
+                Button {} label: {
+                    Label {
+                        Text("Read Books")
+                    } icon: {
+                        Image(systemName: "book")
+                    }
+                  
+                }
+                .tint(Color("yellow").opacity(0.7))
+               
+
             }
-            VStack {}
+            VStack {
+                
+                Button {} label: {
+                    Label {
+                        Text("Read Books")
+                    } icon: {
+                        Image(systemName: "book")
+                    }
+                  
+                }
+                .tint(Color("lightBlue"))
+            }
             HStack {}
         }
+        .labelStyle(.titleFirst)
+        .buttonStyle(.borderedProminent)
+        .padding()
     }
 }
 
 struct PopularHabitView_Previews: PreviewProvider {
-    static var previews: some View { PopularHabitView()
+    static var previews: some View {
+        PopularHabitView()
     }
 }
