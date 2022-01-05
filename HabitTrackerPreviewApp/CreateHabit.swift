2242d84ed0f28db579a9947dae58c64336e53157
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-01-05 08:37:32 +0000

    fixed xcode issues after update

--- HabitTrackerPreviewApp/CreateHabit.swift
+++ HabitTrackerPreviewApp/CreateHabit.swift
@@ -7,46 +7,7 @@ public struct TextFieldHabitStyle: TextFieldStyle {
     }
 }
 
-public struct HabitListItemLabelStyle: LabelStyle {
-    private let backgroundColor: Color
-    private let color: Color
-    private let subtitle: String
 
-    public init(
-        background: Color = Color("green"),
-        color: Color = Color("green"),
-        subtitle: String = "")
-    {
-        self.backgroundColor = background
-        self.color = color
-        self.subtitle = subtitle
-    }
-
-    public func makeBody(configuration: Configuration) -> some View {
-        HStack {
-            configuration.icon
-                .foregroundColor(.white)
-                .frame(width: 40, height: 40)
-                .background(RoundedRectangle(cornerSize: .init(width: 12, height: 12)))
-
-            VStack(alignment: .leading) {
-                Text("subtitle")
-                    .font(.callout.weight(.light))
-                configuration.title
-            }
-        }
-        .padding(.vertical, 10)
-        .font(.body.bold())
-
-        .foregroundColor(Color("blue"))
-    }
-}
-
-public extension LabelStyle where Self == HabitListItemLabelStyle {
-    static func habitListItem(background: Color = Color("green"),
-                              color: Color = Color("green"),
-                              subtitle: String = "") -> Self { .init(background: background, color: color, subtitle: subtitle) }
-}
 
 public extension TextFieldStyle where Self == TextFieldHabitStyle {
     static var bold: Self { .init() }
@@ -62,16 +23,26 @@ struct CreateHabit: View {
                 }
 
                 Section {
-                    List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { _ in
+                    List(0 ..< 5) { _ in
                         NavigationLink {
                             Text("")
                         } label: {
-                            Label {
-                                Text("Swift")
-                            } icon: {
+                            HStack {
                                 Image(systemName: "swift")
+                                    .foregroundColor(.white)
+                                    .frame(width: 40, height: 40)
+                                    .background(RoundedRectangle(cornerSize: .init(width: 12, height: 12)))
+
+                                VStack(alignment: .leading) {
+                                    Text("Swift")
+                                    Text("subtitle")
+                                        .font(.callout.weight(.light))
+                                }
                             }
-                            .labelStyle(.habitListItem(background: .pink, color: .green, subtitle: "some text"))
+                            .padding(.vertical, 10)
+                            .font(.body.bold())
+                            .foregroundColor(Color("blue"))
+                        
                         }
                     }
                 }
