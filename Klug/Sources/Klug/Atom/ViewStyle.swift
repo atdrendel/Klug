1f817368895472e0f38d2d08b4117b14eff8788d
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-01-07 00:36:27 +0000

    minor api design changes

--- Klug/Sources/Klug/Atom/ViewStyle.swift
+++ Klug/Sources/Klug/Atom/ViewStyle.swift
@@ -1,6 +1,72 @@
 import SwiftUI
 
-//extension View {
+public extension View {
+    func border<C>(_ edges: Edge.Set, length: CGFloat? = nil, _ content: C, width: CGFloat = 1, cornerRadius: CGFloat = 10) -> some View where C: ShapeStyle {
+        padding(edges, length).overlay(RoundedRectangle(cornerRadius: cornerRadius).strokeBorder(content, lineWidth: width))
+    }
+
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, _ transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+
+    @ViewBuilder func `else`<Content: View>(_ condition: Bool, _ transform: (Self) -> Content) -> some View {
+        if !condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
+
+public enum Direction {
+    case horizontal, vertical
+}
+
+public extension View {
+    // center view horizontally
+    func center(direction: Direction = .horizontal) -> some View {
+        Group {
+            switch direction {
+            case .horizontal:
+                HStack {
+                    Spacer()
+                    self
+                    Spacer()
+                }
+            case .vertical:
+                VStack {
+                    Spacer()
+                    self
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+public extension View {
+    func navigation() -> some View {
+        NavigationView { self }
+    }
+}
+
+// usage
+// VStack {
+//
+// }.embedInNavigationView()
+
+// AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2274509804, green: 0.4, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.2156862745, green: 1, blue: 0.6235294118, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.9176470588, blue: 0.1960784314, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.2039215686, blue: 0.2745098039, alpha: 1))]), center: .center)
+//    .mask(Image(systemName: "circle.fill").resizable().font(.system(size: 66)))
+//    .frame(width: 66, height: 66)
+//    .if(!isLiteMode, transform: { view in
+//        view.blur(radius: 10)
+//    })
+
+// extension View {
 //    // If condition is met, apply modifier, otherwise, leave the view untouched
 //    public func conditionalModifier<T>(_ condition: Bool, _ modifier: T) -> some View where T: ViewModifier {
 //        Group {
@@ -22,59 +88,4 @@ import SwiftUI
 //            }
 //        }
 //    }
-//}
-
-public extension View {
-    
-    func border<C>(_ edges: Edge.Set, length: CGFloat? = nil, _ content: C, width: CGFloat = 1, cornerRadius: CGFloat = 10) -> some View where C : ShapeStyle {
-        padding(edges, length).overlay(RoundedRectangle(cornerRadius: cornerRadius).strokeBorder(content, lineWidth: width))
-    }
-    
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, _ transform: (Self) -> Content) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-    
-    @ViewBuilder func `else`<Content: View>(_ condition: Bool, _ transform: (Self) -> Content) -> some View {
-        if !condition {
-            transform(self)
-        } else {
-            self
-        }
-    }
-}
-
-extension View {
-    
-    // center view horizontally
-    func centerHorizontally() -> some View {
-        HStack {
-            Spacer()
-            self
-            Spacer()
-        }
-    }
-    
-}
-
-extension View {
-    func navigationView() -> some View {
-        NavigationView { self }
-    }
-}
-
-// usage
-//VStack {
-//
-//}.embedInNavigationView()
-
-
-//AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2274509804, green: 0.4, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.2156862745, green: 1, blue: 0.6235294118, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.9176470588, blue: 0.1960784314, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.2039215686, blue: 0.2745098039, alpha: 1))]), center: .center)
-//    .mask(Image(systemName: "circle.fill").resizable().font(.system(size: 66)))
-//    .frame(width: 66, height: 66)
-//    .if(!isLiteMode, transform: { view in
-//        view.blur(radius: 10)
-//    })
+// }
